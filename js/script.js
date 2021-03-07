9e9440f71c8d7bc2443f0995dc454c46e9e38bb6
koras <kos6476@yandex.ru>
2021-03-07 12:06:06 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,18 @@
-console.log('12')
-    // 2. Создание переменной request
+let template = `
+<div class="card-mini" id="${id}">
+    <div class="card-mini-preview">
+        <img src="./images/${img}.jpg" />
+    </div>
+    <div class="card-info">
+        <div class="card-mini-title">
+            ${title}
+        </div>
+        <div class="card-mini-cost">
+        ${cost} ₽
+        </div>
+    </div>
+</div>`;
 
-var body = ['\r\n'];
-var boundary = String(Math.random()).slice(2);
-var boundaryMiddle = '--' + boundary + '\r\n';
-var boundaryLast = '--' + boundary + '--\r\n'
-
-for (var key in data) {
-    // добавление поля
-    body.push('Content-Disposition: form-data; name="' + key + '"\r\n\r\n' + data[key] + '\r\n');
-}
-body = body.join(boundaryMiddle) + boundaryLast;
-
-
-var request = new XMLHttpRequest();
-// 3. Настройка запроса
-request.open('GET', 'https://praktikum.tk/cohort12/cards', true);
-// 4. Подписка на событие onreadystatechange и обработка его с помощью анонимной функции
-request.addEventListener('readystatechange', function() {
-    // если состояния запроса 4 и статус запроса 200 (OK)
-    if ((request.readyState == 4) && (request.status == 200)) {
-        // например, выведем объект XHR в консоль браузера
-        console.log(request);
-        // и ответ (текст), пришедший с сервера в окне alert
-        console.log(request.responseText);
-        // получить элемент c id = welcome
-        var welcome = document.getElementById('welcome');
-        // заменить содержимое элемента ответом, пришедшим с сервера
-        welcome.innerHTML = request.responseText;
-    }
+document.getElementById('myButton').addEventListener('click', function() {
+    alert(123123);
 });
-
-
-// 5. Отправка запроса на сервер
-// coment test 2
-
-// я бы поправил, но не хочу
-request.send(body);
